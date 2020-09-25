44458fe3f95dccb0fdb7e531390c6cee68e3e5b5
Benoit Hamelin <benoit.hamelin@gmail.com>
2020-09-25 20:54:12 +0000

    Bump version to 1.0
    Signed-off-by: Benoit Hamelin <benoit.hamelin@gmail.com>

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 from setuptools import setup, find_packages
 
 
-DESC = """\
-TBD
-"""
-
 setup(
     name="nano-editorconfig",
-    version="0.0.1",
+    version="1.0",
     packages=find_packages(),
     scripts=["nec"],
     author="Benoit Hamelin",
     author_email="benoit@benoithamelin.com",
     description="Wrapper around GNU nano to enable EditorConfig features",
-    long_description=DESC,
     license="MIT",
     install_requires=["editorconfig"],
     url="https://github.com/hamelin/nano-editorconfig",
